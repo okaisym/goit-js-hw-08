8bb4f2482b46532578596b075897605dd34bf955
okaisym <okaisim@gmail.com>
2023-09-13 00:35:32 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+const markup = createMarkup(galleryItems);
+
+container.insertAdjacentHTML('beforeend', markup);
+
+function createMarkup(arr) {
+    return arr
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                        <div class="gallery__caption">${description}</div>
+                    </a>`;
+        })
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[name="email"]');
+const messageInput = document.querySelector('[name="message"]');
+const savedData = localStorage.getItem('feedback-form-state') || {};
+
+const onInput = function () {
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,  
+    };
+    localStorage.setItem('feedback-form-state', data.value);
+};
+
+if (savedData) {
+    emailInput.value = savedData.email || '';
+    messageInput.value = savedData.message || '';
+} else {
+    emailInput.value = '';
+    messageInput.value = '';
+}
+
+formEl.addEventListener('submit', throttle(onInput, 500));
+formEl.addEventListener('submit', function () {
+    console.log(formData)});
 
